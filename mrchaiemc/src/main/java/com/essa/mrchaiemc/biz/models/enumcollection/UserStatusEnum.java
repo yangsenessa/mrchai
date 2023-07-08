297f1d6915478ca61b65f73020a4dc1c55312f44
yansgenessa <yang82101@163.com>
2023-07-08 09:42:44 +0800

    pipeline with model_base_info save or update

---
+++ mrchaiemc/src/main/java/com/essa/mrchaiemc/biz/models/enumcollection/UserStatusEnum.java
@@ -0,0 +1,65 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2023 All Rights Reserved.
+ */
+package com.essa.mrchaiemc.biz.models.enumcollection;
+
+/**
+ * @author senyang
+ * @version : UserProfileEnum.java, v 0.1 2023年06月06日 12:09 AM senyang Exp $
+ */
+public enum UserStatusEnum {
+    /** 待激活*/
+    UN_AUTHTOKEN("UN_AUTHTOKEN","UN_AUTHTOKEN"),
+    /** 冻结*/
+    FREEZE("FREEZE","FREEZE"),
+    /** 正常*/
+    NORMAL("NORMAL","NORMAL")
+    ;
+
+
+    private String code;
+
+    private String desc;
+
+    UserStatusEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    /**
+     * Getter method for property <tt>code</tt>.
+     *
+     * @return property value of code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * Setter method for property <tt>code</tt>.
+     *
+     * @param code value to be assigned to property code
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     * Getter method for property <tt>desc</tt>.
+     *
+     * @return property value of desc
+     */
+    public String getDesc() {
+        return desc;
+    }
+
+    /**
+     * Setter method for property <tt>desc</tt>.
+     *
+     * @param desc value to be assigned to property desc
+     */
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
